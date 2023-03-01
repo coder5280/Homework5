14c20cd664e8098e574b1d38fdea9591e9b90c55
azamat Khadikov <khadikovazamat@gmail.com>
2023-03-01 08:15:00 -0700

    Problem34

---
+++ Problem34/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Array Size?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+
+FillArray(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"Array Length is {numbers.Length} , {count} even number(s)");
+
+void FillArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("");
+    Console.WriteLine();
+}
+
